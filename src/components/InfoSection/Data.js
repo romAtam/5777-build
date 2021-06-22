ca743f0f6f4207afec609dab67aec3c0c7682898
romAtam <romanatamanchuk5777@gmail.com>
2021-06-22 22:30:15 +0300

    object with info

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -1,4 +1,6 @@
-import image from "../../images/svg-1.svg";
+import image1 from "../../images/svg-1.svg";
+import image2 from "../../images/svg-3.svg";
+import image3 from "../../images/svg-4.svg";
 export const homeObjOne = {
   id: "about",
   lightBg: false,
@@ -10,7 +12,41 @@ export const homeObjOne = {
     "Get access to our exlusive app that allows you to send unlimited transactions without getting charged any fees",
   buttonlabel: "Get started",
   imgStart: false,
-  img: image,
+  img: image1,
+  alt: "car",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
+export const homeObjTwo = {
+  id: "discover",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Premium Bank",
+  headline: "Unlimited Transactions with zero fees",
+  description:
+    "Get access to our exlusive app that allows you to send unlimited transactions without getting charged any fees",
+  buttonlabel: "Get started",
+  imgStart: true,
+  img: image2,
+  alt: "piggi",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
+export const homeObjthree = {
+  id: "about",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Premium Bank",
+  headline: "Unlimited Transactions with zero fees",
+  description:
+    "Get access to our exlusive app that allows you to send unlimited transactions without getting charged any fees",
+  buttonlabel: "Get started",
+  imgStart: false,
+  img: image3,
   alt: "car",
   dark: true,
   primary: true,
